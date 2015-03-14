31585a4ccc4e17409684f3be41b55d57045ee692
Rick McFarley <rickmcfarley@gmail.com>
2015-03-13 22:02:57 -0400

    Added chunk of code from candles mod, by darkrose
    I added candle related code from the candles mod by darkrose(a large chunk of init.lua from the mod https://github.com/darkrose/minetest_mods/tree/master/candles)
    
    I also added a code snippet associating the code with the GNU General Public License version 3 or later.

--- init.lua
+++ init.lua
@@ -1,3 +1,24 @@
+
+-- init.lua
+-- bees + candles minetest mod, by rickmcfarley
+-- Copyright (C) Rick McFarley 2015 <rickmcfarley@gmail.com>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+-- See the GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
+
+--The below code (until line 818) was copied from:
 --Bees
 ------
 --Author	Bas080
@@ -794,3 +815,439 @@
     end
 
 print('[Mod]Bees Loaded!')
+
+
+--The below code was copied from:
+-- init.lua
+-- candles minetest mod, by darkrose
+-- Copyright (C) Lisa Milne 2013 <lisa@ltmnet.com>
+
+local candles = {};
+
+candles.types = {
+	{
+		unlit = 'bees:candle',
+		lit = 'bees:candle_lit',
+		name = 'Candle',
+		ingot = nil,
+		image = 'candles_candle'
+	},
+	{
+		unlit = 'bees:candle_wall_steel',
+		lit = 'bees:candle_wall_steel_lit',
+		name = 'Steel Wall-Mount Candle',
+		ingot = 'default:steel_ingot',
+		image = 'candles_candle_steel'
+	},
+	{
+		unlit = 'bees:candle_wall_copper',
+		lit = 'bees:candle_wall_copper_lit',
+		name = 'Copper Wall-Mount Candle',
+		ingot = 'moreores:copper_ingot',
+		image = 'candles_candle_copper'
+	},
+	{
+		unlit = 'bees:candle_wall_silver',
+		lit = 'bees:candle_wall_silver_lit',
+		name = 'Silver Wall-Mount Candle',
+		ingot = 'moreores:silver_ingot',
+		image = 'candles_candle_silver'
+	},
+	{
+		unlit = 'bees:candle_wall_gold',
+		lit = 'bees:candle_wall_gold_lit',
+		name = 'Gold Wall-Mount Candle',
+		ingot = 'moreores:gold_ingot',
+		image = 'candles_candle_gold'
+	},
+	{
+		unlit = 'bees:candle_wall_bronze',
+		lit = 'bees:candle_wall_bronze_lit',
+		name = 'Bronze Wall-Mount Candle',
+		ingot = 'moreores:bronze_ingot',
+		image = 'candles_candle_bronze'
+	},
+	{
+		unlit = 'bees:candelabra_steel',
+		lit = 'bees:candelabra_steel_lit',
+		name = 'Steel Candelebra',
+		ingot = 'default:steel_ingot',
+		image = 'candles_candelabra_steel'
+	},
+	{
+		unlit = 'bees:candelabra_copper',
+		lit = 'bees:candelabra_copper_lit',
+		name = 'Copper Candelebra',
+		ingot = 'moreores:copper_ingot',
+		image = 'candles_candelabra_copper'
+	},
+	{
+		unlit = 'bees:candelabra_silver',
+		lit = 'bees:candelabra_silver_lit',
+		name = 'Silver Candelebra',
+		ingot = 'moreores:silver_ingot',
+		image = 'candles_candelabra_silver'
+	},
+	{
+		unlit = 'bees:candelabra_gold',
+		lit = 'bees:candelabra_gold_lit',
+		name = 'Gold Candelebra',
+		ingot = 'moreores:gold_ingot',
+		image = 'candles_candelabra_gold'
+	},
+	{
+		unlit = 'bees:candelabra_bronze',
+		lit = 'bees:candelabra_bronze_lit',
+		name = 'Bronze Candelebra',
+		ingot = 'moreores:bronze_ingot',
+		image = 'candles_candelabra_bronze'
+	},
+}
+
+candles.find_lit = function(name)
+	for i,n in ipairs(candles.types) do
+		if n.unlit == name then
+			return n.lit
+		end
+	end
+	return nil
+end
+
+candles.find_unlit = function(name)
+	for i,n in ipairs(candles.types) do
+		if n.lit == name then
+			return n.unlit
+		end
+	end
+	return nil
+end
+
+candles.light = function(pos, node, puncher)
+	if not puncher or not node then
+		return
+	end
+	local wield = puncher:get_wielded_item()
+	if not wield then
+		return
+	end
+	wield = wield:get_name()
+	if wield and wield == 'default:torch' then
+		local litname = candles.find_lit(node.name)
+		minetest.env:add_node(pos,{name=litname, param1=node.param1, param2=node.param2})
+	end
+end
+
+candles.snuff = function(pos, node, puncher)
+	if not puncher or not node then
+		return
+	end
+	local wield = puncher:get_wielded_item()
+	if not wield then
+		return
+	end
+	wield = wield:get_name()
+	if not wield or wield ~= 'default:torch' then
+		local unlitname = candles.find_unlit(node.name)
+		minetest.env:add_node(pos,{name=unlitname, param1=node.param1, param2=node.param2})
+	end
+end
+
+candles.create_wall = function(ctype)
+	minetest.register_node(ctype.unlit, {
+		description = ctype.name,
+		tile_images = {ctype.image.."_top.png",ctype.image.."_top.png",ctype.image..".png"},
+		drawtype = "nodebox",
+		paramtype = "light",
+		paramtype2 = "facedir",
+		groups = {crumbly=3,oddly_breakable_by_hand=1},
+		on_punch = candles.light,
+		sunlight_propagates = true,
+		walkable = false,
+		node_box = {
+			type = "fixed",
+			fixed = {
+				-- x,y,z,x,y,z
+				-- body
+				{0.11, -0.3, -0.07, 0.27, 0.2, 0.07},
+				-- wick
+				{0.18, 0.2, -0.02, 0.22, 0.3, 0.02},
+				-- holder
+				{0.1, -0.4, -0.1, 0.3, -0.27, 0.1},
+				{0.1, -0.35, -0.05, 0.5, -0.3, 0.05},
+				{0.45, -0.40, -0.12, 0.5, -0.25, 0.12},
+			},
+		},
+		selection_box = {
+			type = "fixed",
+			fixed = {0.05, -0.45, -0.2, 0.5, 0.3, 0.15}
+		},
+		on_place = function(itemstack, placer, pointed_thing)
+			local above = pointed_thing.above
+			local under = pointed_thing.under
+			local dir = {x = under.x - above.x,
+				     y = under.y - above.y,
+				     z = under.z - above.z}
+
+			local wdir = minetest.dir_to_wallmounted(dir)
+			local fdir = minetest.dir_to_facedir(dir)
+
+			if wdir == 0 or wdir == 1 then
+				return itemstack
+			else
+				fdir = fdir-1
+				if fdir < 0 then
+					fdir = 3
+				end
+				minetest.env:add_node(above, {name = itemstack:get_name(), param2 = fdir})
+				itemstack:take_item()
+				return itemstack
+			end
+		end
+	})
+
+	minetest.register_node(ctype.lit, {
+		description = ctype.name,
+		tile_images = {ctype.image.."_top.png",ctype.image.."_top.png",ctype.image.."_lit.png"},
+		drawtype = "nodebox",
+		paramtype = "light",
+		paramtype2 = "facedir",
+		groups = {},
+		on_punch = candles.snuff,
+		sunlight_propagates = true,
+		walkable = false,
+		light_source = 8,
+		drop = ctype.unlit,
+		node_box = {
+			type = "fixed",
+			fixed = {
+				-- x,y,z,x,y,z
+				-- body
+				{0.11, -0.3, -0.07, 0.27, 0.2, 0.07},
+				-- wick
+				{0.18, 0.2, -0.02, 0.22, 0.42, 0.02},
+				-- flame
+				{0.14, 0.25, -0.06, 0.26, 0.3, 0.06},
+				{0.16, 0.3, -0.04, 0.24, 0.37, 0.04},
+				-- holder
+				{0.1, -0.4, -0.1, 0.3, -0.27, 0.1},
+				{0.1, -0.35, -0.05, 0.5, -0.3, 0.05},
+				{0.45, -0.40, -0.12, 0.5, -0.25, 0.12},
+			},
+		},
+		selection_box = {
+			type = "fixed",
+			fixed = {0.05, -0.45, -0.2, 0.5, 0.45, 0.15}
+		},
+		can_dig = function(pos,player)
+			return false
+		end,
+	})
+	minetest.register_craft({
+		output = ctype.unlit,
+		recipe = {
+			{'bees:candle'},
+			{ctype.ingot},
+		}
+	})
+end
+
+candles.create_candelabra = function(ctype)
+	minetest.register_node(ctype.unlit, {
+		description = ctype.name,
+		tile_images = {ctype.image.."_top.png",ctype.image.."_top.png",ctype.image..".png"},
+		drawtype = "nodebox",
+		paramtype = "light",
+		paramtype2 = "facedir",
+		groups = {crumbly=3,oddly_breakable_by_hand=1},
+		on_punch = candles.light,
+		sunlight_propagates = true,
+		walkable = false,
+		node_box = {
+			type = "fixed",
+			fixed = {
+				-- x,y,z,x,y,z
+				-- body
+				{-0.08, -0.3, -0.48, 0.08, 0.2, -0.32},
+				{-0.08, -0.3, -0.08, 0.08, 0.2, 0.08},
+				{-0.08, -0.3, 0.32, 0.08, 0.2, 0.48},
+				-- wick
+				{-0.02, 0.2, -0.42, 0.02, 0.3, -0.38},
+				{-0.02, 0.2, -0.02, 0.02, 0.3, 0.02},
+				{-0.02, 0.2, 0.38, 0.02, 0.3, 0.42},
+				-- holder
+				{-0.1, -0.27, -0.5, 0.1, -0.2, -0.3},
+				{-0.1, -0.27, -0.1, 0.1, -0.2, 0.1},
+				{-0.1, -0.27, 0.3, 0.1, -0.2, 0.5},
+				-- stand
+				{-0.05, -0.32, -0.45, 0.05, -0.27, 0.45},
+				-- base
+				{-0.05, -0.45, -0.05, 0.05, -0.3, 0.05},
+				{-0.1, -0.49, -0.1, 0.1, -0.45, 0.1},
+				{-0.15, -0.5, -0.15, 0.15, -0.49, 0.15},
+			},
+		},
+		on_place = function(itemstack, placer, pointed_thing)
+			local above = pointed_thing.above
+			local under = pointed_thing.under
+			local dir = {x = under.x - above.x,
+				     y = under.y - above.y,
+				     z = under.z - above.z}
+
+			local wdir = minetest.dir_to_wallmounted(dir)
+			local fdir = minetest.dir_to_facedir(dir)
+
+			if wdir == 1 then
+				minetest.env:add_node(above, {name = itemstack:get_name(), param2 = fdir})
+				itemstack:take_item()
+			end
+			return itemstack
+		end
+	})
+
+	minetest.register_node(ctype.lit, {
+		description = ctype.name,
+		tile_images = {ctype.image.."_top.png",ctype.image.."_top.png",ctype.image.."_lit.png"},
+		drawtype = "nodebox",
+		paramtype = "light",
+		paramtype2 = "facedir",
+		groups = {},
+		on_punch = candles.snuff,
+		sunlight_propagates = true,
+		walkable = false,
+		light_source = 12,
+		drop = ctype.unlit,
+		node_box = {
+			type = "fixed",
+			fixed = {
+				-- x,y,z,x,y,z
+				-- body
+				{-0.08, -0.3, -0.48, 0.08, 0.2, -0.32},
+				{-0.08, -0.3, -0.08, 0.08, 0.2, 0.08},
+				{-0.08, -0.3, 0.32, 0.08, 0.2, 0.48},
+				-- wick
+				{-0.02, 0.2, -0.42, 0.02, 0.45, -0.38},
+				{-0.02, 0.2, -0.02, 0.02, 0.45, 0.02},
+				{-0.02, 0.2, 0.38, 0.02, 0.45, 0.42},
+				-- flame
+				{-0.06, 0.25, -0.46, 0.06, 0.3, -0.34},
+				{-0.04, 0.3, -0.44, 0.04, 0.37, -0.36},
+				{-0.06, 0.25, -0.06, 0.06, 0.3, 0.06},
+				{-0.04, 0.3, -0.04, 0.04, 0.37, 0.04},
+				{-0.06, 0.25, 0.34, 0.06, 0.3, 0.46},
+				{-0.04, 0.3, 0.36, 0.04, 0.37, 0.44},
+				-- holder
+				{-0.1, -0.27, -0.5, 0.1, -0.2, -0.3},
+				{-0.1, -0.27, -0.1, 0.1, -0.2, 0.1},
+				{-0.1, -0.27, 0.3, 0.1, -0.2, 0.5},
+				-- stand
+				{-0.05, -0.32, -0.45, 0.05, -0.27, 0.45},
+				-- base
+				{-0.05, -0.45, -0.05, 0.05, -0.3, 0.05},
+				{-0.1, -0.49, -0.1, 0.1, -0.45, 0.1},
+				{-0.15, -0.5, -0.15, 0.15, -0.49, 0.15},
+			},
+		},
+		can_dig = function(pos,player)
+			return false
+		end,
+	})
+	minetest.register_craft({
+		output = ctype.unlit,
+		recipe = {
+			{'bees:candle','bees:candle','bees:candle'},
+			{ctype.ingot,ctype.ingot,ctype.ingot},
+		}
+	})
+end
+
+minetest.register_node("bees:candle", {
+	description = "Candle",
+	tile_images = {"candles_candle_top.png","candles_candle.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	groups = {crumbly=3,oddly_breakable_by_hand=1},
+	on_punch = candles.light,
+	sunlight_propagates = true,
+	walkable = false,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			-- x,y,z,x,y,z
+			-- body
+			{-0.08, -0.5, -0.08, 0.08, 0.0, 0.08},
+			-- wick
+			{-0.02, 0.0, -0.02, 0.02, 0.1, 0.02},
+		},
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.1, -0.5, -0.1, 0.1, 0.12, 0.1}
+	},
+	on_place = function(itemstack, placer, pointed_thing)
+		local above = pointed_thing.above
+		local under = pointed_thing.under
+		local dir = {x = under.x - above.x,
+			     y = under.y - above.y,
+			     z = under.z - above.z}
+
+		local wdir = minetest.dir_to_wallmounted(dir)
+
+		if wdir == 1 then
+			minetest.env:add_node(above, {name = 'bees:candle'})
+			itemstack:take_item()
+		end
+		return itemstack
+	end
+})
+
+minetest.register_node("bees:candle_lit", {
+	description = "Candle",
+	tile_images = {"candles_candle_top.png","candles_candle_lit.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	groups = {},
+	on_punch = candles.snuff,
+	sunlight_propagates = true,
+	walkable = false,
+	light_source = 8,
+	drop = 'bees:candle',
+	node_box = {
+		type = "fixed",
+		fixed = {
+			-- x,y,z,x,y,z
+			-- body
+			{-0.08, -0.5, -0.08, 0.08, 0.0, 0.08},
+			-- wick
+			{-0.02, 0.0, -0.02, 0.02, 0.25, 0.02},
+			-- flame
+			{-0.06, 0.1, -0.06, 0.06, 0.15, 0.06},
+			{-0.04, 0.15, -0.04, 0.04, 0.22, 0.04},
+		},
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.1, -0.5, -0.1, 0.1, 0.37, 0.1}
+	},
+	can_dig = function(pos,player)
+		return false
+	end,
+})
+
+minetest.register_craft({
+	output = 'bees:candle',
+	recipe = {
+		{'bees:wax','farming:string','bees:wax'},
+	}
+})
+
+for i,n in ipairs(candles.types) do
+	if n.ingot then
+		if string.find(n.unlit,'candelabra') then
+			candles.create_candelabra(n)
+		else
+			candles.create_wall(n)
+		end
+	end
+end
+
+print('[Mod]Beeswax Candles Loaded!')
